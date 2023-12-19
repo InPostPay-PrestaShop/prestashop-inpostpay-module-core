f250e22cb213b2e1d1a8098767b89b87061b78e6
Michał Kaleta <michal.kaleta@waynet.pl>
2023-12-19 18:49:08 +0100

    remove unused file

--- lib/interfaces/LoggerInterface.php
+++ lib/interfaces/LoggerInterface.php
@@ -4,24 +4,9 @@ namespace izi\interfaces;
 
 interface LoggerInterface
 {
-    /**
-     * @param string $message
-     */
     public static function log(string $message);
 
-    /**
-     * @param string $message
-     * @param string $header
-     */
     public static function response(string $message, string $header = '');
 
-    /**
-     * @param string $url
-     * @param string $method
-     * @param string $payload
-     * @param int $length
-     * @param string $response
-     * @param int $code
-     */
     public static function request(string $url, string $method, string $payload, int $length, string $response, int $code);
 }
