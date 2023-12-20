c50e363bf9d17ace8801d4f7699a2cc22dcece6e
Michał Kaleta <michal.kaleta@waynet.pl>
2023-12-20 12:18:31 +0100

    cart view update: changes from the dev build

--- _dev/src/index.js
+++ _dev/src/index.js
@@ -19,10 +19,38 @@ window.iziGetPayData = iziGetPayData;
 window.INPOST_PAY_PRESTASHOP_VERSION = settings.version_presta;
 
 function handleDOMLoaded() {
+  let storeButton;
+  if (window.document.querySelector(".page-cart")) {
+    storeButton = window.document.querySelector(".cart-grid #inpostizi_block_home");
+  }
+  if (storeButton) {
+    storeButton = storeButton.cloneNode(true);
+  }
+
   if (typeof prestashop !== "undefined") {
     updateCountOnLoad(prestashop.cart.products_count);
     prestashop.on("updateCart", function (event) {
-      updateCount(event?.resp?.cart.products_count || prestashop.cart.products_count || 0);
+      const count = event?.resp?.cart.products_count || prestashop.cart.products_count || null;
+
+      if (null !== count) {
+        updateCount(count);
+      }
+    });
+
+    prestashop.on("updatedCart", () => {
+      if (window.document.querySelector(".page-cart")) {
+        const cartButton = window.document.querySelector(".cart-grid #inpostizi_block_home");
+        if (!cartButton) {
+          const target = window.document.querySelector(".checkout.cart-detailed-actions");
+          if (target) {
+            target.append(storeButton);
+            window.handleInpostIziButtons();
+            setTimeout(updateWithFetch, 1000);
+          }
+        }
+      } else {
+        updateWithFetch();
+      }
     });
 
     prestashop.on("updatedProduct", function (event) {
@@ -30,18 +58,36 @@ function handleDOMLoaded() {
     });
   }
 
-  let cartButton = window.document.querySelector('.cart-grid  #inpostizi_block_home');
+  let cartButton = window.document.querySelector('.cart-grid #inpostizi_block_home');
   if (cartButton) {
     let target = window.document.querySelector('.checkout.cart-detailed-actions');
     if (target) {
       target.append(cartButton);
-      cartButton.remove();
-      let cartButtons = window.document.querySelectorAll('.cart-grid  #inpostizi_block_home');
+      let cartButtons = window.document.querySelectorAll('.cart-grid #inpostizi_block_home');
       if (cartButtons.length > 1) {
         cartButtons[0].remove();
       }
     }
   }
+}
+
+function updateWithFetch() {
+  const baseURI = window.location.origin;
+  $.ajax({
+    url: baseURI + "/index.php?fc=module&module=inpostizi&controller=cart",
+    type: "GET",
+    dataType: "json",
+    success: (data) => {
+      if (data.count) {
+        updateCount(data.count);
+      } else {
+        updateCount(0);
+      }
+    },
+    error: (error) => {
+      console.error("Error retrieving cart details: ", error);
+    }
+  });
 }
 
 function updateCount(count = 0) {
