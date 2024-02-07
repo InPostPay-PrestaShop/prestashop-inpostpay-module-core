008d6889c9e4a3f31673d0bc4c5bcbd8bf4c986d
Igor Stępień <igor@istepien.dev>
2024-02-07 16:10:58 +0100

    Legacy translation system for twig template

---
+++ src/Twig/Extension/LegacyTranslation.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace izi\prestashop\Twig\Extension;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class LegacyTranslation extends AbstractExtension
+{
+    private $module;
+
+    public function __construct(\Module $module)
+    {
+        $this->module = $module;
+    }
+
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('legacy_trans', [$this, 'legacyTrans']),
+        ];
+    }
+
+    public function legacyTrans($value)
+    {
+        return $this->module->l($value, 'admin_template_translations');
+    }
+}
