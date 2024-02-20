b267d4ddb93b255b284fe4e8b588e0699d6575ac
Michał Kaleta <michal.kaleta@waynet.pl>
2024-02-20 15:09:47 +0100

    increment module version

---
+++ upgrade/upgrade-1.5.3.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+/**
+ * @param \InPostIzi $module
+ *
+ * @return bool
+ */
+function upgrade_module_1_5_3(\Module $module)
+{
+    \Tools::clearSf2Cache('prod');
+
+    return true;
+}
