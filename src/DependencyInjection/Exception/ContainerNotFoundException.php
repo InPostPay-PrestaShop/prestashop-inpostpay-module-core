96c4eda621e329d1171142bcf13097f4bbd70058
Michał Kaleta <michal.kaleta@waynet.pl>
2024-03-06 16:16:00 +0100

    container access before controller initialization fix

---
+++ src/DependencyInjection/Exception/ContainerNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace izi\prestashop\DependencyInjection\Exception;
+
+final class ContainerNotFoundException extends \RuntimeException
+{
+    public static function create(\Exception $previous = null): self
+    {
+        return new self('DI container was not found.', 0, $previous);
+    }
+}
