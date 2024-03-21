bfd900cda93b30d018cdd0aa352162c22973bea9
Michał Kaleta <michal.kaleta@waynet.pl>
2024-03-21 17:08:46 +0100

    IP-51 PS < 1.7.5 binding confirmation listeners removed on "cartUpdate" event fix

---
+++ upgrade/upgrade-1.5.7.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+/**
+ * @param \InPostIzi $module
+ *
+ * @return bool
+ */
+function upgrade_module_1_5_7(\Module $module)
+{
+    if (\Tools::version_compare('_PS_VERSION_', '1.7.5')) {
+        \Media::clearCache();
+    }
+
+    return true;
+}
