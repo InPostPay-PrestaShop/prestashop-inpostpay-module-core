100a951c71f0815c0a33f36fd49bf11e5cb03269
Michał Kaleta <michal.kaleta@waynet.pl>
2025-07-31 13:02:29 +0000

    feat: [DV-1354] add a custom order data webservice endpoint

---
+++ src/Webservice/Model/InPostOrderTrait.php
@@ -0,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+namespace izi\prestashop\Webservice\Model;
+
+use izi\prestashop\ObjectModel\ObjectManagerInterface;
+use izi\prestashop\Repository\OrderDataRepositoryInterface;
+use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
+
+/**
+ * @internal
+ *
+ * @mixin InPostOrder
+ */
+trait InPostOrderTrait
+{
+    public function getWebserviceParameters($ws_params_attribute_name = null): array
+    {
+        $this->webserviceParameters['fields']['inpost_point'] = [
+            'getter' => 'getWsInPostPoint',
+            'setter' => false,
+        ];
+
+        return parent::getWebserviceParameters($ws_params_attribute_name);
+    }
+
+    public function getWsInPostPoint(): ?string
+    {
+        if ('inpostizi' === $this->module) {
+            return $this->getPointIdFromModuleData();
+        }
+
+        if (null !== $pointId = $this->getPointIdFromBaseLinkerData()) {
+            return $pointId;
+        }
+
+        if (!is_callable(['parent', 'getWsInPostPoint'])) {
+            return null;
+        }
+
+        return parent::getWsInPostPoint();
+    }
+
+    private function getPointIdFromModuleData(): ?string
+    {
+        try {
+            $data = $this->get(OrderDataRepositoryInterface::class)->getOrderData((string) $this->id);
+        } catch (ServiceNotFoundException $e) {
+            return null;
+        }
+
+        if (null === $data) {
+            return null;
+        }
+
+        return $data->getDelivery()->getPoint();
+    }
+
+    private function getPointIdFromBaseLinkerData(): ?string
+    {
+        if (!$this instanceof \BaseLinkerOrder) {
+            return null;
+        }
+
+        if (!$this->bl_delivery_point_id) {
+            return null;
+        }
+
+        if (0 >= $carrierId = (int) $this->id_carrier) {
+            return null;
+        }
+
+        try {
+            $carrier = $this->get(ObjectManagerInterface::class)->find(\Carrier::class, (int) $carrierId);
+        } catch (ServiceNotFoundException $e) {
+            return null;
+        }
+
+        if (null === $carrier) {
+            return null;
+        }
+
+        if (!preg_match('/paczkomat|inpost/i', $carrier->name)) {
+            return null;
+        }
+
+        return $this->bl_delivery_point_id;
+    }
+
+    /**
+     * @template T
+     *
+     * @param class-string<T> $id
+     *
+     * @return T
+     */
+    private function get(string $id)
+    {
+        $module = \Module::getInstanceByName('inpostizi');
+        assert($module instanceof \InPostIzi);
+
+        return $module->get($id);
+    }
+}
---
+++ upgrade/upgrade-2.3.0.php
@@ -0,0 +1,50 @@
+<?php
+
+use InPost\Izi\Upgrade\CacheClearer;
+use izi\prestashop\Hook\Webservice\AddWebserviceResources;
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+require_once __DIR__ . '/CacheClearer.php';
+
+class InPostIziUpdater_2_3_0
+{
+    /**
+     * @var Module
+     */
+    private $module;
+
+    public function __construct(Module $module)
+    {
+        $this->module = $module;
+    }
+
+    public static function create(Module $module): self
+    {
+        return new self($module);
+    }
+
+    public function upgrade(): bool
+    {
+        CacheClearer::getInstance()->clear();
+
+        return $this->registerHooks();
+    }
+
+    private function registerHooks(): bool
+    {
+        return $this->module->registerHook([
+            AddWebserviceResources::HOOK_NAME,
+        ]);
+    }
+}
+
+/**
+ * @param InPostIzi $module
+ */
+function upgrade_module_2_3_0(Module $module): bool
+{
+    return InPostIziUpdater_2_3_0::create($module)->upgrade();
+}
