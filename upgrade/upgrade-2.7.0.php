67822d06f27faf58c7bb445228eb67b99a6cb8a5
Michał Kaleta <michal.kaleta@waynet.pl>
2026-06-15 12:25:04 +0200

    chore(upgrade): clean up stale assets and deprecation logs
    (cherry picked from commit 7464cbf4ed32aa9c30f72fe37bee36d186dc0710)

--- upgrade/upgrade-2.7.0.php
+++ upgrade/upgrade-2.7.0.php
@@ -1,40 +1,57 @@
 <?php
 
+use InPost\Izi\Upgrade\AssetsRemoverTrait;
 use izi\prestashop\CacheClearer\SymfonyCacheClearer;
 use izi\prestashop\Hook\HookExecutor;
+use Symfony\Component\Finder\Finder;
 
 if (!defined('_PS_VERSION_')) {
     exit;
 }
 
+require_once __DIR__ . '/AssetsRemoverTrait.php';
+
 class InPostIziUpdater_2_7_0
 {
-    /**
-     * @var Module
-     */
-    private $module;
+    use AssetsRemoverTrait;
+
+    private const STALE_ASSETS = [
+        'js/front/v2.2bd48b321391b1c84487.js',
+    ];
 
     /**
      * @var bool
      */
     private $disableHotProducts;
 
-    public function __construct(Module $module, bool $disableHotProducts)
+    /**
+     * @var string
+     */
+    private $logsDir;
+
+    public function __construct(Module $module, bool $disableHotProducts, string $logsDir)
     {
         $this->module = $module;
         $this->disableHotProducts = $disableHotProducts;
+        $this->logsDir = $logsDir;
     }
 
     public static function create(Module $module): self
     {
-        return new self($module, !getenv('INPOST_IZI_HOT_PRODUCTS_ENABLED'));
+        $logsDir = Tools::version_compare(_PS_VERSION_, '1.7.4')
+            ? _PS_ROOT_DIR_ . '/app/logs/inpost/'
+            : _PS_ROOT_DIR_ . '/var/logs/inpost/';
+
+        return new self($module, !getenv('INPOST_IZI_HOT_PRODUCTS_ENABLED'), $logsDir);
     }
 
     public function upgrade(): bool
     {
         SymfonyCacheClearer::getInstance()->clear();
 
-        return $this->unregisterHotProductHooks();
+        return $this->unregisterHotProductHooks()
+            && $this->removeDeprecationLogs()
+            && $this->removeStaleAssets(self::STALE_ASSETS);
     }
 
     private function unregisterHotProductHooks(): bool
@@ -49,6 +66,18 @@ class InPostIziUpdater_2_7_0
         }
 
         return (bool) $result;
+    }
+
+    private function removeDeprecationLogs(): bool
+    {
+        $files = Finder::create()
+            ->in($this->logsDir)
+            ->name('izi.deprecations*.log')
+            ->files();
+
+        $this->getFileSystem()->remove($files);
+
+        return true;
     }
 }
 
