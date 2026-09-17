08b6e35a0e62c23293c335e999a6b0c8d11edbb9
Igor Stępień <igor.stepien@waynet.pl>
2026-09-17 13:37:51 +0200

    chore: module version bump

--- inpostizi.php
+++ inpostizi.php
@@ -60,7 +60,7 @@ class InPostIzi extends PaymentModule implements WidgetInterface
     public function __construct()
     {
         $this->name = 'inpostizi';
-        $this->version = '3.4.4';
+        $this->version = '3.4.5';
         $this->author = 'InPost S.A.';
         $this->tab = 'payments_gateways';
 
